3e598d4a36c8ecc20d4820dc9345ac10ff3c5493
Vukasin Micic <vukasin.micic5@gmail.com>
2020-11-23 18:20:32 +0100

    Add restaurant list screen

---
+++ components/restaurantItem.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+
+export default function RestaurantItem({ item }) {
+    return (
+        <View style={styles.restaurantItem}>
+            <Text>
+                {item.name}
+            </Text>
+            <Text>
+                {item.description}
+            </Text>
+            <Text>
+                {item.address}
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    restaurantItem: {
+        padding: 16,
+        marginTop: 16,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 10
+    }
+});
--- screens/restaurants.js
+++ screens/restaurants.js
@@ -1,14 +1,53 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+
+import RestaurantItem from '../components/restaurantItem'
 
 export default function Restaurants() {
   [resturants, setRestaurants] = useState([
-    { key: '1', name: 'La fresh'}
+    { key: '1', name: 'La fresh', description: 'Best pizza', address: 'Brace Ribnikar 10' },
+    { key: '2', name: 'Sef', description: 'Best sendwichees', address: 'Brace Ribnikar 3' },
+    { key: '3', name: 'Kao nekad', description: 'Best cooked meals', address: 'Danila Kisa 4' }
   ])
 
   return (
-    <View>
-      <Text>All restaurants</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>
+          Restaurants
+        </Text>
+      </View>
+      <View style={styles.content}>
+        <FlatList
+          data={resturants}
+          renderItem={({ item }) => (
+            <RestaurantItem item={item}/>
+          )} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'aqua',
+  },
+  content: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: 'yellow',
+  },
+  header: {
+    paddingTop: 10,
+    backgroundColor: 'white'
+  },
+  headerText: {
+    paddingBottom: 10,
+    textAlign: 'center',
+    color: 'black',
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
+});

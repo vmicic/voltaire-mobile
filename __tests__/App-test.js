af75914cb0b041b4a4b3f09533c6eb9fb9ced421
vmicic <vukasin.micic5@gmail.com>
2020-12-09 08:48:26 +0100

    Add detox

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -8,7 +8,15 @@ import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import MenuItem from '../components/MenuItem';
+
+let menuItem = {
+  name: 'da',
+  price: '200',
+  description: 'hej',
+};
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const tree = renderer.create(<MenuItem menuItem={menuItem} />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
